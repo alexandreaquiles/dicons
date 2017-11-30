5d0d86f48e05c70f0e4ff8221f82c65cd951986a
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2017-11-30 15:29:19 -0200

    usando log4j ao invés de sysout

--- src/br/com/dicons/service/Dirs.java
+++ src/br/com/dicons/service/Dirs.java
@@ -2,11 +2,14 @@ package br.com.dicons.service;
 
 import java.io.File;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dirs {
+
+	private static final Logger logger = Logger.getLogger(Dirs.class);
 
 	private File diconsDir;
 	private File diconsZipsDir;
@@ -16,7 +19,7 @@ public class Dirs {
 		this.diconsDir = new File(usuarioDirName + File.separator + "dicons");
 		this.diconsZipsDir = new File(usuarioDirName + File.separator + "dicons-zips");
 		if (!this.diconsZipsDir.exists()) {
-			System.out.println("Criando diretório: " + this.diconsZipsDir.getPath());
+			logger.warn("Criando diretório: " + this.diconsZipsDir.getPath());
 			this.diconsZipsDir.mkdirs();
 		}
 	}
